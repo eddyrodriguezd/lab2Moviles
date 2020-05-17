52433634958fda5c9aa066a71142e756a62a54e9
Eddy Rodriguez <e.rodriguezd@pucp.edu.pe>
2020-05-17 16:44:11 -0500

    getApiKey

--- app/src/main/java/com/tel306/lab2/ListaTrabajosActivity.java
+++ app/src/main/java/com/tel306/lab2/ListaTrabajosActivity.java
@@ -3,12 +3,65 @@ package com.tel306.lab2;
 import androidx.appcompat.app.AppCompatActivity;
 import static com.tel306.lab2.Util.isInternetAvailable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.tel306.lab2.entidades.Trabajo;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ListaTrabajosActivity extends AppCompatActivity {
+
+    private String apiKey;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_trabajos);
+        getApiKey();
+    }
+
+    public void getApiKey(){
+        if (isInternetAvailable(this)){
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+            String url = "http://ec2-54-165-73-192.compute-1.amazonaws.com:9000/getApiKey?groupKey=HTUxbtfKpEb2GJ3Y2d9e";
+            StringRequest stringRequest = new StringRequest(StringRequest.Method.POST, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Log.d("Api-key", response);
+                    JSONObject jsonObject = null;
+                    try {
+                        jsonObject = new JSONObject(response);
+                        apiKey = jsonObject.getString("api-key");
+                        Log.d("Api-key", apiKey);
+                        Toast.makeText(ListaTrabajosActivity.this, apiKey, Toast.LENGTH_LONG).show();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.d("Api-key", error.getLocalizedMessage());
+                }
+            });
+            requestQueue.add(stringRequest);
+        }
+
     }
 }
