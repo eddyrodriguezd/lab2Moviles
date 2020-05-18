5db9d5ae68bdf94816052681c66726525606c6a2
Eddy Rodriguez <e.rodriguezd@pucp.edu.pe>
2020-05-17 19:10:27 -0500

    Menu

--- app/src/main/java/com/tel306/lab2/ListaEmpleadosActivity.java
+++ app/src/main/java/com/tel306/lab2/ListaEmpleadosActivity.java
@@ -1,8 +1,14 @@
 package com.tel306.lab2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import static com.tel306.lab2.Util.isInternetAvailable;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class ListaEmpleadosActivity extends AppCompatActivity {
 
@@ -10,5 +16,27 @@ public class ListaEmpleadosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_empleados);
+        setTitle("Empleados");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_app_bar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch(item.getItemId()){
+
+            case R.id.itemChangeList:
+                startActivity(new Intent(this, ListaTrabajosActivity.class));
+                finish();
+            case R.id.itemAdd:
+                Toast.makeText(this, "Agregar", Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/tel306/lab2/ListaTrabajosActivity.java
+++ app/src/main/java/com/tel306/lab2/ListaTrabajosActivity.java
@@ -1,12 +1,17 @@
 package com.tel306.lab2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import static com.tel306.lab2.Util.isInternetAvailable;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -40,6 +45,7 @@ public class ListaTrabajosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_trabajos);
+        setTitle("Trabajos");
 
         getApiKey(new VolleyCallBack() {
             @Override
@@ -60,6 +66,27 @@ public class ListaTrabajosActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_app_bar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch(item.getItemId()){
+
+            case R.id.itemChangeList:
+                startActivity(new Intent(this, ListaEmpleadosActivity.class));
+                finish();
+            case R.id.itemAdd:
+                Toast.makeText(this, "Agregar", Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public void getApiKey(final VolleyCallBack callBack){
